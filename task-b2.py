b756073f28bf02158aadad97400d02acd4e04877
Lahiru Madhusanka <hhlahirumadusanka@gmail.com>
2026-01-24 16:52:24 +0200

    Fix camera resolution issue

--- task-b2.py
+++ task-b2.py
@@ -13,16 +13,16 @@ CURRENT_DATE = "10/12/2025"
 # Object 1: A Blue Box
 OBJ1_LABEL = "Box"
 # (Top-Left X, Top-Left Y, Bottom-Right X, Bottom-Right Y)
-OBJ1_RECT_COORDS = (33, 80, 50, 95)
+OBJ1_RECT_COORDS = (182, 240, 230, 285)
 OBJ1_COLOR = (0, 0, 255)  # BGR Red
-OBJ1_TEXT_COORDS = (30, 110)  # Text below the object
+OBJ1_TEXT_COORDS = (180, 225)  # Text below the object
 
 # Object 2: A Red Cone
 OBJ2_LABEL = "Cone"
 # (Center X, Center Y, Radius)
-OBJ2_CIRCLE_COORDS = (44, 50, 13)
+OBJ2_CIRCLE_COORDS = (212, 150, 35)
 OBJ2_COLOR = (255, 0, 0)  # BGR Blue
-OBJ2_TEXT_COORDS = (30, 35)  # Text above the object
+OBJ2_TEXT_COORDS = (190, 100)  # Text above the object
 
 # 1. Load the image
 img = cv2.imread(INPUT_FILE)
@@ -34,9 +34,9 @@ date_text = f"Date: {CURRENT_DATE}"
 cv2.putText(
     img,
     title_text,
-    (2, 7),                        # Starting position (X, Y)
+    (10, 10),                        # Starting position (X, Y)
     cv2.FONT_HERSHEY_SIMPLEX,      # Font type
-    0.3,                           # Font scale
+    0.5,                           # Font scale
     (0, 0, 0),                     # Color: Black (BGR)
     1,                             # Thickness
     cv2.LINE_AA                    # Line type for smoothness
@@ -44,9 +44,9 @@ cv2.putText(
 cv2.putText(
     img,
     date_text,
-    (2, 17),                        # Starting position (X, Y)
+    (10, 30),                       # Starting position (X, Y)
     cv2.FONT_HERSHEY_SIMPLEX,      # Font type
-    0.2,                           # Font scale
+    0.4,                           # Font scale
     (0, 0, 0),                     # Color: Black (BGR)
     1,                             # Thickness
     cv2.LINE_AA                    # Line type for smoothness
@@ -54,7 +54,7 @@ cv2.putText(
 
 # --- Object 1 Annotation (Rectangle) ---
 x1, y1, x2, y2 = OBJ1_RECT_COORDS
-cv2.rectangle(img, (x1, y1), (x2, y2), OBJ1_COLOR, 1)
+cv2.rectangle(img, (x1, y1), (x2, y2), OBJ1_COLOR, 2)
 cv2.putText(
     img,
     OBJ1_LABEL,
@@ -68,7 +68,7 @@ cv2.putText(
 
 # --- Object 2 Annotation (Cone) ---
 center_x, center_y, radius = OBJ2_CIRCLE_COORDS
-cv2.circle(img, (center_x, center_y), radius, OBJ2_COLOR, 1)
+cv2.circle(img, (center_x, center_y), radius, OBJ2_COLOR, 2)
 cv2.putText(
     img,
     OBJ2_LABEL,
